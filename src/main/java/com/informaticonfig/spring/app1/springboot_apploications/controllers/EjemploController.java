43a69ea106483f046a5b9818e16b01d552e3c109
carlos manjarres <carlosmanjarresmunoz@gmail.com>
2025-08-05 10:12:57 -0500

    estructura mvc terminada

--- src/main/java/com/informaticonfig/spring/app1/springboot_apploications/controllers/EjemploController.java
+++ src/main/java/com/informaticonfig/spring/app1/springboot_apploications/controllers/EjemploController.java
@@ -3,16 +3,18 @@ package com.informaticonfig.spring.app1.springboot_apploications.controllers;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import com.informaticonfig.spring.models.Empleados;
 @Controller
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info(Map<String, Object> model){
-        model.put("Titulo", "servidor en linea");
-        model.put("servidor", "informaticonfig");
-        model.put("Ip", "1.0.0");
+
+    public String info(Model model){
+        Empleados empleado1= new Empleados("carlos", "manjarres", "Calle Falsa 123", "Desarrollador", 30, 123456789, 1);
+        
+        model.addAttribute("Empleado", empleado1);
         return "detalles_info";
     }
 
--- src/main/java/com/informaticonfig/spring/app1/springboot_apploications/controllers/EjemploRestController.java
+++ src/main/java/com/informaticonfig/spring/app1/springboot_apploications/controllers/EjemploRestController.java
@@ -2,20 +2,25 @@ package com.informaticonfig.spring.app1.springboot_apploications.controllers;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.HashMap;
+import com.informaticonfig.spring.models.Empleados;
 
 @RestController
+@RequestMapping("/api")
 public class EjemploRestController {
 
-    @GetMapping("/detalles_info2")
+    @GetMapping(path = "/detalles_info2")
+
     public Map<String, Object> detalles_info2(){
+        Empleados empleado1= new Empleados("Juan", "Pérez", "Calle Falsa 123", "Desarrollador", 30, 123456789, 1);
+
         Map<String, Object> respuesta = new HashMap<>();
-        respuesta.put("Titulo", "servidor en linea");
-        respuesta.put("servidor", "informaticonfig");
-        respuesta.put("Ip", "1.0.0");
+        respuesta.put("Empleado", empleado1);
         return respuesta;
     }
 
